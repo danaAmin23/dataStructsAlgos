51d671e37be4c4c9ee79bf257d90fd5ca9a31fcf
Dana Amin <46908984+danaAmin23@users.noreply.github.com>
2022-01-03 17:07:12 -0500

    Update isUniqueChars.java

--- CCI/isUniqueChars.java
+++ CCI/isUniqueChars.java
@@ -28,7 +28,35 @@ class Result {
         
             //if char is in array return false  
         return bool;
-    }
+    }// ens is UniqueChar
+    
+   //alternative way 
+        /*
+     checks to see if string has unique characters
+     */
+
+    public static boolean isUniqueChar2(String n) {
+        //loop through string,and char array 
+        boolean bool = true;
+        boolean charSet[] = new boolean[128];
+    
+            //loop through string,and char array 
+            for(int i=0;i<n.length();i++){
+                //get char code of char 
+                int charCode = n.charAt(i);
+                //if we found the element at index charCode we already found it 
+                if(charSet[charCode]){
+                    return false;
+                }//end if 
+                else{
+                    
+                    charSet[charCode]=true;
+                }//end else 
+            }//end for 
+        
+            //if char is in array return false  
+        return true;
+    }//end isUnique2
     
 }// end result 
 
@@ -36,7 +64,7 @@ public class Solution {
     public static void main(String[] args) throws IOException {        
        System.out.println(Result.isUniqueChar("ABCDE--"));
        System.out.println(Result.isUniqueChar("12324"));
-       System.out.println(Result.isUniqueChar("aAbCc"));
+       System.out.println(Result.isUniqueChar2("aAbCc"));
 
     }
 }
